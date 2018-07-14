80988de0b27dbf868e2739e711c19238761b866e
5argon <pyasry@gmail.com>
2018-07-14 12:42:39 +0900

    First commit. #StupidHackTH

---
+++ GrooveCoasterControllerClient/app/src/main/java/info/s5argon/groovecoastercontroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package info.s5argon.groovecoastercontroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
